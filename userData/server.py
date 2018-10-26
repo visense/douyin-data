a1baa77c28dd8bd97cae6c58eca6293a0be00f7f
PUGE <100284685@qq.com>
2018-10-26 22:02:23 +0800

    完成根据ID爬取用户信息

--- userData/server.py
+++ userData/server.py
@@ -24,6 +24,42 @@ import requests
 # conn.commit()
 # conn.close()
 
+# 抖音密码字典
+mapList = {
+    'xe60d': '0',
+    'xe616': '0',
+    'xe610': '2',
+    'xe61a': '3',
+    'xe618': '1',
+    'xe611': '3',
+    'xe60e': '1',
+    'xe602': '1',
+    'xe605': '2',
+    'xe617': '2',
+    'xe613': '7',
+    'xe604': '3',
+    'xe60c': '4',
+    'xe619': '4',
+    'xe606': '4',
+    'xe607': '5',
+    'xe61b': '5',
+    'xe60f': '5',
+    'xe608': '6',
+    'xe61f': '6',
+    'xe612': '6',
+    'xe603': '0',
+    'xe61c': '7',
+    'xe60a': '7',
+    'xe60b': '8',
+    'xe615': '9',
+    'xe614': '8',
+    'xe61d': '8',
+    'xe61e': '9',
+    'xe609': '9',
+    '.': '.',
+    'w': 'w'
+  }
+
 # 截取字符串
 def subString(str, start, end, startInd = 0):
   startIndex = str.find(start.decode('utf-8'), startInd)
@@ -53,91 +89,64 @@ def subStringArr(str, start, end):
   # endIndex = str.index(end.decode('utf-8'), startIndex + 1)
   # print(endIndex)
 
+## 字体解码方法
 def fontDecrypt(arr):
-  mapList1 = {
-    'xe60c': '4',
-    'xe60d': '0',
-    'xe60d': '0'
-  }
-  mapList = {
-    'xe60d': '0',
-    'xe616': '0',
-    'xe610': '0',
-    'xe61a': '0',
-    'xe611': '0',
-    'xe602': '1',
-    'xe605': '2',
-    'xe617': '2',
-    'xe613': '7',
-    'xe604': '3',
-    'xe60c': '4',
-    'xe619': '4',
-    'xe606': '4',
-    'xe607': '5',
-    'xe608': '6',
-    'xe61f': '6',
-    'xe612': '6',
-    'xe603': '7',
-    'xe61c': '7',
-    'xe60a': '7',
-    'xe60b': '8',
-    'xe615': '8',
-    'xe61e': '8',
-    'xe609': '9',
-    '.': '.',
-    'w': 'w'
-  }
   str = ''
   for item in arr:
-    print item
-    if (mapList[item]):
+    if (item in mapList):
       str += mapList[item]
     else:
-      str += item
+      print item
   return str
+
+## 数字字体特殊处理
+def subFontHtml(str, start):
+  html = subString(res.text, start, '<span class="text"')
+  # 针对小数点做特殊处理
+  html = html.replace('.', '&#.;')
+  # 针对万做特殊处理
+  html = html.replace('</i>w </span>', '&#w;')
+  font = subStringArr(html, '&#', ';')
+  return fontDecrypt(font)
+
+# 抖音ID特殊处理
+def subDouyinID(str):
+  str = subString(str, '抖音ID：     ', '</p>')
+  # 清洗掉所有空格
+  str = str.replace(' ', '')
+  # 清洗出文本
+  str = str.replace('<iclass="iconiconfont">', '')
+  str = str.replace('</i>', '')
+  # 将字体清洗为字符串
+  for fontKey in mapList:
+    str = str.replace('&#' + fontKey + ';', mapList[fontKey])
+  return str
+
 # 没有headers会返回页面不存在
 headers = {
   "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
 }
-res = requests.get('https://www.douyin.com/share/user/58803201952', headers = headers)
+res = requests.get('https://www.douyin.com/share/user/6796248446', headers = headers)
 res.encoding = 'utf-8'
 
 # print(res.text)
 # 名字
+
 print(subString(res.text, 'class="nickname">', '</p>'))
 # 抖音ID
-print(subString(res.text, '抖音ID：     ', '<i'))
+print(subDouyinID(res.text))
+
 # 签名
 # print(subString(res.text, '"signature">', '</p>'))
 # 城市
 print(subString(res.text, '"location">', '</span>'))
 # 星座
 print(subString(res.text, '"constellation">', '</span>'))
-
 # 关注
-focusHtml = subString(res.text, 'class="focus block">', '<span class="text"')
-# 针对小数点做特殊处理
-focusHtml = focusHtml.replace('.', '&#.;')
-# 针对万做特殊处理
-focusHtml = focusHtml.replace('</i>w </span>', '&#w;')
-focusFont = subStringArr(focusHtml, '&#', ';')
-print(fontDecrypt(focusFont))
-
+print(subFontHtml(res.text, 'class="focus block">'))
 # 粉丝
-followerHtml = subString(res.text, 'class="follower block">', '<span class="text"')
-# 针对小数点做特殊处理
-followerHtml = followerHtml.replace('.', '&#.;')
-# 针对万做特殊处理
-followerHtml = followerHtml.replace('</i>w </span>', '&#w;')
-followerFont = subStringArr(followerHtml, '&#', ';')
-print(fontDecrypt(followerFont))
-
+print(subFontHtml(res.text, 'class="follower block">'))
 # 粉丝
-likedHtml = subString(res.text, 'class="liked-num block">', '<span class="text"')
-# 针对小数点做特殊处理
-likedHtml = likedHtml.replace('.', '&#.;')
-# 针对万做特殊处理
-likedHtml = likedHtml.replace('</i>w </span>', '&#w;')
-likedFont = subStringArr(likedHtml, '&#', ';')
-print(fontDecrypt(likedFont))
-# subString('sdssdaardsddddddd1515', 'sdaar', '1515')
+print(subFontHtml(res.text, 'class="liked-num block">'))
+
+# 作品
